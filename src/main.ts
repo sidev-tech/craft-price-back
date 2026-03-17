7246692668af6e2217e98adde5b0b558c5a3082b
Iryna Kolesnyk <iryna.kolesnyk@valsydev.com>
2026-03-17 23:32:18 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +20,23 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const frontendUrl = process.env.FRONTEND_URL;
+  const frontendUrlLocal = process.env.FRONTEND_URL_LOCAL;
+
+  const allowedOrigins = [frontendUrl, frontendUrlLocal];
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'X-Requested-With',
+      'Origin',
+    ],
+    credentials: true,
+  });
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
